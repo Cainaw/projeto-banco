091824153dfb0ce752203456b9c647e9405d50dd
Cainã <cainamwc@gmail.com>
2025-01-28 03:26:04 -0300

    modificada a classe de controle de bonificação

--- ProjetoBanco/src/banco/ControleDeBonificacao.java
+++ ProjetoBanco/src/banco/ControleDeBonificacao.java
@@ -3,20 +3,18 @@ package banco;
 import gerencia.*;
 
 public class ControleDeBonificacao {
-	public void bonificar(Funcionario [] funcionario) {
-		
+	public void bonificar(Funcionario [] funcionarios) {
+		for (Funcionario funcionario : funcionarios) {
+			Double novoSalario = funcionario.getSalario() * (funcionario.getBonificacao() + 1.0);
+			funcionario.setSalario(novoSalario);
+		}
 	}
 	
 	public double getBonificacao(Funcionario funcionario) {
 		return funcionario.getBonificacao();
 	}
 
-	public void setBonificacao(String funcao, double bonificacao) {
-		Funcionario funcionario;
-		
-		if (funcao.equalsIgnoreCase((funcionario = (new Funcionario(null, null, null))).getFuncao())) {
-			funcionario.setBonificacao(bonificacao);
-			return;
-		}
+	public void setBonificacao(Funcionario funcionario, double bonificacao) {
+		funcionario.setBonificacao(bonificacao);
 	}
 }
