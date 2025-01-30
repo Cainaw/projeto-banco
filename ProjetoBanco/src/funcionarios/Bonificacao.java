e124d98562442f4feb139d79cad588404eef7971
Cainã <cainamwc@gmail.com>
2025-01-30 10:04:34 -0300

    modificações concluidas

--- ProjetoBanco/src/funcionarios/Bonificacao.java
+++ ProjetoBanco/src/funcionarios/Bonificacao.java
@@ -1,6 +1,6 @@
 package funcionarios;
 
-public class Bonificacao {
+public abstract class Bonificacao {
 	public Double bonificacao;
 	public Double salario;
 
@@ -8,21 +8,47 @@ public class Bonificacao {
 		this.bonificacao = 0.0;
 		this.salario = 0.0;
 	}
+	
+	public void bonificar() {
+		try {
+			this.salario += this.bonificacao;
+		}
+		catch (Exception e) {
+			System.err.println("Erro ao tentar bonificar: " + e.getMessage());
+		}
+	}
+	
+	public Boolean aumentarBonificacao(Double valor) {
+		try {
+			if (valor > 0.0) {
+				this.bonificacao += valor;
+				return true;
+			} return false;
+		}
+		catch (Exception e) {
+			System.err.println("Erro ao aumentar bonificação: " + e.getMessage());
+			return null;
+		}
+	}
+	
+	public Boolean diminuirBonificacao(Double valor) {
+		try {
+			if (valor > 0.0) {
+				this.bonificacao -= valor;
+				return true;
+			} return false;
+		}
+		catch (Exception e) {
+			System.err.println("Erro ao diminuir bonificação: " + e.getMessage());
+			return null;
+		}
+	}
 
 	public Double getBonificacao() {
 		return bonificacao;
-	}
-
-	public void setBonificacao(Double bonificacao) {
-		this.bonificacao = bonificacao;
 	}
 	
 	public Double getSalario() {
 		return salario;
 	}
-
-	public void setSalario(Double salario) {
-		this.salario = salario;
-	}
-	
 }
